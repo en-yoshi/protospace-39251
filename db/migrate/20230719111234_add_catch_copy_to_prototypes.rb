4b2652a13d610ab0450ae6bd157b1df9d32ecfce
en-yoshi <endo.y.1989@gmail.com>
2023-07-24 22:32:29 +0900

    delete database

--- db/migrate/20230719111234_add_catch_copy_to_prototypes.rb
+++
@@ -1,5 +0,0 @@
-class AddCatchCopyToPrototypes < ActiveRecord::Migration[6.0]
-  def change
-    add_column :prototypes, :catch_copy, :string
-  end
-end
--- db/migrate/20230720063445_add_image_url_to_prototypes.rb
+++
@@ -1,5 +0,0 @@
-class AddImageUrlToPrototypes < ActiveRecord::Migration[6.0]
-  def change
-    add_column :prototypes, :image_url, :string
-  end
-end
